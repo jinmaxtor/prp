9d803d8d983b59eb32ec90241e5985c9d253d22b
jin_maxtor <jin_maxtor@outlook.com>
2018-10-03 03:26:12 -0400

    Corregido package.json

--- config/local.js
+++ config/local.js
@@ -14,6 +14,7 @@ module.exports = {
     // options or custom configuration specifically for your app (e.g. Stripe, Mailgun, etc.)
     host: process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1",
     port: process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    environment: process.env.NODE_ENV || 'development'
+    environment: 'development'
+    //environment: process.env.NODE_ENV || 'development'
 
 };
